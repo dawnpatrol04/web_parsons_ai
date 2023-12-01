3a90ce3a92e2d3b3ec44c58a6d5d2edd3590d915
dawnpatrol04 <dawnpatrol04@gmail.com>
2023-12-01 02:55:37 -0500

    Added functionality to fetch and load the header component on page load.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-/* Leave blank for now */
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('components/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading the header:', error));
+});
